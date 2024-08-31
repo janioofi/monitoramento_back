3754622a776835dc92da7265e9370a4c66bab448
Jânio <janioofi@gmail.com>
2024-08-30 21:44:37 -0300

    first commit

---
+++ src/main/java/com/janioofi/monitoramento/MonitoramentoApplication.java
@@ -0,0 +1,13 @@
+package com.janioofi.monitoramento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoApplication.class, args);
+	}
+
+}
